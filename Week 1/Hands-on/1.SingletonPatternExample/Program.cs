343ac9cab6651a04ca0d33de1a1e2bc9f2ebdfbb
Ayush Samad <maxala7@gmail.com>
2025-06-22 00:56:13 +0530

    Design Patterns and Principles Example 1 done

---
+++ Week 1/Hands-on/1.SingletonPatternExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SingletonPatternExample;
+
+namespace SingletonPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger1 = Logger.GetInstance();
+            Logger logger2 = Logger.GetInstance();
+
+            logger1.Log("First log message.");
+            logger2.Log("Second log message.");
+
+            if (object.ReferenceEquals(logger1, logger2))
+            {
+                Console.WriteLine("Both are the same instance. Singleton success.");
+            }
+            else
+            {
+                Console.WriteLine("Both have different instences. Singleton failed.");
+            }
+        }
+    }
+}
